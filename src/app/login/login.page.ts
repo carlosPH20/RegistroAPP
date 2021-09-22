6b9d7d2a1ac0149e750641aefb45a6a7d7136b7d
carlosPH20 <85924683+carlosPH20@users.noreply.github.com>
2021-09-22 04:40:46 -0300

    avance2

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,8 +14,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formularioLogin: FormGroup;
 
+  constructor(public fb: FormBuilder) {
+
+    this.formularioLogin = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required)
+    })
+
+   }
   ngOnInit() {
   }
 
