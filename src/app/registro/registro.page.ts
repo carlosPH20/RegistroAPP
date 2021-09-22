79f5428cfe55b0c8d3ef5c9b4391eaa4aa554e7e
carlosPH20 <85924683+carlosPH20@users.noreply.github.com>
2021-09-22 05:39:49 -0300

    casi finalizado

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +15,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  formularioRegistro: FormGroup;
+
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController,
+    public navCtrl: NavController) { 
+
+    this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required),
+      'confirmacionPassword': new FormControl("",Validators.required)
+    })
+
+  }
 
   ngOnInit() {
   }
 
+  async guardar(){
+    var f = this.formularioRegistro.value;
+    
+  
+    if(this.formularioRegistro.invalid){
+      const alert = await this.alertController.create({
+        header: 'Campos incompletos',
+        message: 'Debes rellenarlos campos requeridos',
+        buttons: ['Confirmar']});  
+      await alert.present();
+      return;
+    }
+    else{
+      this.navCtrl.navigateRoot('login');
+    }
+      var usuario = {
+        nombre: f.nombre,
+        password: f.password
+      }
+      localStorage.setItem('usuario',JSON.stringify(usuario));
+  }  
 }
